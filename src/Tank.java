7f7683091230bf4e91a95133225dc99ff11f51cb
Joao Victor Rutz Crespo <jv.rutz97@gmail.com>
2025-12-07 10:22:00 -0300

    getters and setter addicted
    getters e setter addicted and system level too

--- src/Tank.java
+++ src/Tank.java
@@ -1,19 +1,30 @@
 public class Tank {
 
-    private int life = 10;
-    private Double damageNOW = 2;
-    private int damage = 2;
+    //Atributos do tank
+    final Double Baselife = 10.0;
+    private Double life;
+    final Double Basedamage = 3.0;
+    private Double damage;
+    private int Speed = 1;
     private int level = 1;
+    private int xp = 0;
+    private int xpUP = 2;
+    //private int kill = 0;
 
+    //Movimento
     private int row = 13;
     private int col = 6;
     private Direction direction;
 
+
+    //Construtor
     public Tank (int row, int col,  Direction direction ) {
 
         this.row = row;
         this.col = col;
         this.direction = direction;
+        this.life = Baselife;
+        this.damage = Basedamage;
 
     }
 
@@ -33,11 +44,79 @@ public class Tank {
         col++;
     }
 
-    public void Damage(int damage, int level){
-        this.damageNOW = damage*(1+0.1*level) ;
+    //Sistema de XP
+    public void gainXP(int amount) {
+        this.xp += amount;
+
+        if (this.xp == xpUP) {
+            levelUP();
+            this.xp = 0;
+            this.xpUP += 2;
+
+        }
     }
 
-    public void TakeDMG(int damage){
-        this.life -= damage;
+
+    //Sistema de Level
+    private void levelUP(){
+
+        this.level++;
+        this.life = Baselife*(1+0.4*level);
+        this.damage = Basedamage*(1+0.1*level) ;
+
+        if (this.level%2 ==0){
+            Speed++;
+        }
+
     }
+    //Sistema de dano
+    public void Takedamage(int amount){
+        this.life -= damage;
+        if (this.life < 0){
+            this.life = 0.0;
+            System.out.println("Voce morreu!");
+        }
+    }
+
+    public Double getLife() {
+        return life;
+    }
+    public void setLife(Double life) {
+        if(life<0){
+            this.life = 0.0;
+        }else {
+            this.life = life;
+        }
+    }
+
+    public Double getDamage() {
+        return damage;
+    }
+    public void setDamage(Double damage) {
+        this.damage = damage;
+    }
+    public int getSpeed() {
+        return Speed;
+    }
+    public void setSpeed(int speed) {
+        this.Speed = speed;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level){
+        if (level==10){
+            System.out.println("Voce atingiu o nivel maximo");
+            this.level = 10;
+        }else {
+            this.level = level;
+        }
+    }
+    public int getXp() {
+        return xp;
+    }
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
 }
--- src/enemy.java
+++ src/enemy.java
@@ -1,37 +1,18 @@
 public class enemy {
 
-        private int life = 10;
-        private int damage = 2;
-        private int level = 1;
+        private int life = 5;
+        private int damage = 1;
 
-        private int row = 13;
-        private int col = 6;
+        private int row = 1;
+        private int col = 9;
         private Direction direction;
 
-        public Tank (int row, int col,  Direction direction ) {
+        public enemy(int row, int col, Direction direction ) {
 
-            this.row = row;
+            this.row = row ;
             this.col = col;
             this.direction = direction;
 
         }
-
-        public void moveUP(){
-            row--;
-        }
-
-        public void moveDOWN(){
-            row++;
-        }
-
-        public void moveLEFT(){
-            col--;
-        }
-
-        public void moveRIGHT(){
-            col++;
-        }
-
-        public
 
 }
